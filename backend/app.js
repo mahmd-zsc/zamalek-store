f7ea218365a9311f8be15bc46135149155011264
unknown <moma8607914@gmail.com>
2024-05-09 22:19:02 +0300

    edit on vercel.json

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,23 @@
-let express = require("express");
-let dotenv = require("dotenv");
+// Require necessary modules
+const express = require("express");
+const dotenv = require("dotenv");
 const { connectWithDb } = require("./config/db");
 const errorHandler = require("./middleware/errorHandler");
-let helmet = require("helmet");
-let cors = require("cors");
-let path = require("path");
-let app = express();
-app.use(express.json());
+const helmet = require("helmet");
+const cors = require("cors");
+const path = require("path");
+
+// Initialize Express app
+const app = express();
+
+// Load environment variables
 dotenv.config({ path: ".env" });
+
+// Connect to the database
 connectWithDb();
-app.use(errorHandler);
+
+// Middleware
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "images")));
 app.use(helmet());
 app.use(cors());
@@ -19,11 +27,16 @@ app.use(`${process.env.API_VERSION}categories/`, require("./routes/category"));
 app.use(`${process.env.API_VERSION}sizes/`, require("./routes/size"));
 app.use(`${process.env.API_VERSION}products/`, require("./routes/product"));
 app.use(`${process.env.API_VERSION}brands/`, require("./routes/brand"));
-app.use(`${process.env.API_VERSION}users/`, require("./routes/user")); // Add the user route
-app.use(`${process.env.API_VERSION}auth/`, require("./routes/auth")); // Add the auth route
-app.use(`${process.env.API_VERSION}colors/`, require("./routes/color")); // Add the auth route
-app.use(`${process.env.API_VERSION}orders/`, require("./routes/order")); // Add the auth route
+app.use(`${process.env.API_VERSION}users/`, require("./routes/user"));
+app.use(`${process.env.API_VERSION}auth/`, require("./routes/auth"));
+app.use(`${process.env.API_VERSION}colors/`, require("./routes/color"));
+app.use(`${process.env.API_VERSION}orders/`, require("./routes/order"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+// Error handling middleware
+app.use(errorHandler);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
