65251045b811f92dfe3713af622a9e75bf2170bc
unknown <moma8607914@gmail.com>
2024-05-10 19:27:13 +0300

    set fronend inside server for deploy

---
+++ server/server.js
@@ -0,0 +1,48 @@
+// Require necessary modules
+const express = require("express");
+const dotenv = require("dotenv");
+const { connectWithDb } = require("./config/db");
+const errorHandler = require("./middleware/errorHandler");
+const helmet = require("helmet");
+const cors = require("cors");
+const path = require("path");
+
+// Initialize Express app
+const app = express();
+
+// Load environment variables
+dotenv.config({ path: ".env" });
+
+// Connect to the database
+connectWithDb();
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "images"))); // Serving images
+app.use(helmet());
+app.use(cors());
+
+// Routes for entities
+app.use(`${process.env.API_VERSION}categories/`, require("./routes/category"));
+app.use(`${process.env.API_VERSION}sizes/`, require("./routes/size"));
+app.use(`${process.env.API_VERSION}products/`, require("./routes/product"));
+app.use(`${process.env.API_VERSION}brands/`, require("./routes/brand"));
+app.use(`${process.env.API_VERSION}users/`, require("./routes/user"));
+app.use(`${process.env.API_VERSION}auth/`, require("./routes/auth"));
+app.use(`${process.env.API_VERSION}colors/`, require("./routes/color"));
+app.use(`${process.env.API_VERSION}orders/`, require("./routes/order"));
+
+// Serving static files for client build
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+// Error handling middleware (should come after all routes)
+app.use(errorHandler);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
